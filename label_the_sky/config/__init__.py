52203e06e54e73cb8d9e2d7c7bebe80f40456148
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2022-10-14 22:58:28 -0300

    change class numbers (class_map)

--- label_the_sky/config/__init__.py
+++ label_the_sky/config/__init__.py
@@ -1,6 +1,7 @@
 import yaml
 
 BANDS = ['u','f378','f395','f410','f430','g','f515','r','f660','i','f861','z']
-CLASS_MAP = {'GALAXY': 0, 'STAR': 1, 'QSO': 2, 'UNKNOWN': 3}
+#CLASS_MAP = {'GALAXY': 0, 'STAR': 1, 'QSO': 2, 'UNKNOWN': 3}
+CLASS_MAP = {2: 0, 1: 1, 0: 2, 'UNKNOWN': 3} # 0 in the df is QSO (which is 2 in the program), etc... 
 
 config = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
