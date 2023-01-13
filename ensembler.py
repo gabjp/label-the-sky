4520d4be67a92074779ba8746bbac12e53e619bf
Gabriel Jacob Perin <gjperin@yahoo.com.br>
2023-01-13 20:10:59 -0300

    start implementing ensemble

---
+++ ensembler.py
@@ -0,0 +1,49 @@
+from label_the_sky.training.trainer import Trainer, set_random_seeds
+from sklearn.model_selection import StratifiedKFold
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import  LogisticRegression
+import sys
+import pandas as pd
+import numpy as np
+
+CLASS_MAP = {0:2,1:1,2:0} # 0 - Galaxy, 1 - Star, 2 - Quasar
+
+_morph = ['FWHM_n', 'A', 'B', 'KRON_RADIUS']
+_feat = ['u_iso',
+             'J0378_iso',
+             'J0395_iso',
+             'J0410_iso',
+             'J0430_iso',
+             'g_iso',
+             'J0515_iso',
+             'r_iso',
+             'J0660_iso',
+             'i_iso',
+             'J0861_iso',
+             'z_iso']
+
+set_random_seeds()
+
+dataset = sys.argv[1]
+
+dataset_csv = pd.read_csv(dataset + ".csv")
+
+#Fixing csv data target labels (This is pretty ugly, find a better fix later)
+dataset_csv["target"] = dataset_csv["target"].apply(lambda c: CLASS_MAP[c])
+
+train_csv = dataset_csv[(dataset_csv.split=="train")]
+val_csv = dataset_csv[dataset_csv.split=="val"]
+test_csv = dataset_csv[dataset_csv.split=="test"]
+
+# Load tabular data
+X_train_csv, y_train_csv = (train_csv[_morph+_feat], train_csv["target"])
+X_val_csv, y_val_csv = (val_csv[_morph+_feat], val_csv["target"])
+X_test_csv, y_test_csv = (test_csv[_morph+_feat], test_csv["target"])
+
+#Load 12ch image data
+X_train_12ch, y_train_12ch = (np.load(f"~/data/{dataset}_12_X_train.npy"), np.load(f"~/data/{dataset}_12_y_train.npy"))
+X_val_12ch, y_val_12ch = (np.load(f"~/data/{dataset}_12_X_val.npy"), np.load(f"~/data/{dataset}_12_y_val.npy"))
+X_test_12ch, y_test_12ch = (np.load(f"~/data/{dataset}_12_X_test.npy"), np.load(f"~/data/{dataset}_12_y_test.npy"))
+
+print(y_train_csv)
+print(y_train_12ch)
--- label_the_sky/config/__init__.py
+++ label_the_sky/config/__init__.py
@@ -3,5 +3,7 @@ import yaml
 BANDS = ['u','f378','f395','f410','f430','g','f515','r','f660','i','f861','z']
 #CLASS_MAP = {'GALAXY': 0, 'STAR': 1, 'QSO': 2, 'UNKNOWN': 3}
 CLASS_MAP = {2: 0, 1: 1, 0: 2, 'UNKNOWN': 3} # 0 in the df is QSO (which is 2 in the program), etc... 
+# A solução adequada pra essa gambiarra é desfazzer a troca de classes que é feita aqui e fornecer para o programa de geração
+# de dados o csv com a codificaçao correta.
 
 config = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
